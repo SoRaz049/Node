02e01d4e7c7433867cdf5860691bc1ce90c81aca
Swaraj Barnwal <ikku12piku@gmail.com>
2024-02-20 08:52:10 +0545

    Append and reading of file done successfully.

--- file.js
+++ file.js
@@ -1,8 +1,24 @@
 const fs = require('fs');
-const math = require('./maths')
 
 
-fs.writeFileSync('./test.txt','Hey there');
+//fs.writeFileSync('./test.txt','Hey there');  //sync
 
 
+//async
+//fs.writeFile('./test.txt', "HEllo hello", (err)=> {});
 
+
+//  result = fs.readFileSync('./contact.txt',"utf-8");
+// console.log(result);
+
+// fs,fs.readFile('./contact.txt',"utf-8", (err, result) =>{
+//     if(err){
+//         console.log("Error",err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// })
+
+
+fs.appendFileSync('./test.txt', "hey There this is the append process \n");
