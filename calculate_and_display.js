08584a8a74dd7a2063dedb21900c9e82caf19a4e
Swaraj Barnwal <ikku12piku@gmail.com>
2024-02-20 09:13:50 +0545

    Accepr User request values.

--- calculate_and_display.js
+++ calculate_and_display.js
@@ -2,10 +2,20 @@ const fs = require('fs');
 const math = require('./maths')
 
 //fs.writeFileSync('./test.txt','Hey there');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question("Please enter the 1st value: ", value1 =>{
+    readline.question("Please enter the 2nd value: ", value2 => {
+        const result = math.add(parseInt(value1),parseInt(value2));
+        console.log(result);
+        readline.close();
+    });
+});
 
 
- const result = math.add(3,5);
- console.log(result);
 
-fs.writeFileSync('./calc.txt',"The sum is " + String(result));
+// fs.writeFileSync('./calc.txt',"The sum is " + String(result));
 
