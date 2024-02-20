f74ce8a3794f375780ae7a71546dcda1c34289b0
Swaraj Barnwal <ikku12piku@gmail.com>
2024-02-20 09:18:44 +0545

    Added the file append and calculation saved in text file.

--- calculate_and_display.js
+++ calculate_and_display.js
@@ -11,11 +11,13 @@ readline.question("Please enter the 1st value: ", value1 =>{
     readline.question("Please enter the 2nd value: ", value2 => {
         const result = math.add(parseInt(value1),parseInt(value2));
         console.log(result);
+        fs.appendFileSync('./calc.txt', "The new sum for the data "+ value1 + " and " + value2 + " is: " + String(result)+ "\n");
         readline.close();
     });
 });
 
 
 
-// fs.writeFileSync('./calc.txt',"The sum is " + String(result));
+//fs.writeFileSync('./calc.txt',"The sum is " + String(result));
+
 
