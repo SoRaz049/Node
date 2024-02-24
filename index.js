8e5574c6ed217acbf6db1e696e24b834aacae328
Swaraj Barnwal <ikku12piku@gmail.com>
2024-02-24 17:06:12 +0545

    URls and the queries

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: New Req Received \n`;
+    console.log("The URL is: ", req.url)
+    if (req.url === "/favicon.ico") {
+        return res.end();
+    }
+
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+
+    const log = `${Date.now()}: ${req.url} New Req Received \n`;
+    
     fs.appendFile('log.txt', log, (err, data) => {
-        switch (req.url) {
+        if (err) {
+            console.error("Error writing to log.txt", err);
+        }
+
+        switch (myUrl.pathname) {
             case "/":
                 res.end("Hey there from Server");
                 break;
             
             case "/about":
-                res.end("I am Swaraj from Server side.");
+                const username = myUrl.query.myname;
+                res.end(`Hi! ${username}`);
                 break;
             
             default: 
